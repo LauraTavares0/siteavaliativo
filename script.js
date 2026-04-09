bc107c67df80d4629d96e673e49cbe7f33022a2a
LauraTavares0 <laura.7845460@aluno.mg.gov.br>
2026-04-09 07:28:52 -0300

    p

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const profissoes = [
 {
     nome: "Especialista em IA",
-    imagem:"https://www.uol.com.br/tilt/noticias/redacao/2023/05/15/especialista-em-ia-eles-ganham-ate-r-35-mil-por-mes-criando-ias.htm"
+    imagem:"img2.jpg"
     descricao: "Cria inteligências artificiais.",
     curiosidade: "Alta demanda no futuro."
 },
